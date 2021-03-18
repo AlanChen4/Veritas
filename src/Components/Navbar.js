6b63e43518541d2a1353fd775a8fec2bc4d90de5
Alan Chen <avchen4@gmail.com>
2021-03-18 01:57:23 -0400

    Change name of the links on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 function Navigation() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" >
-            <Navbar.Brand href="#home">Veritas Economics</Navbar.Brand>
+            <Navbar.Brand href="home">Veritas Economics</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-nav"/>
             <Navbar.Collapse id="navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href='#home'>Choice Model</Nav.Link>
-                    <Nav.Link href='#contact'>Contact</Nav.Link>
-                    <Nav.Link href='#about'>About</Nav.Link>
-                    <Nav.Link href='#help'>Help</Nav.Link>
+                    <Nav.Link href='home'>Choice Model</Nav.Link>
+                    <Nav.Link href='contact'>Contact</Nav.Link>
+                    <Nav.Link href='about'>About</Nav.Link>
+                    <Nav.Link href='help'>Help</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
