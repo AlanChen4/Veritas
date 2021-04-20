c5c915c2df77639fe6526c75a5b9151c9248e339
Alan Chen <avchen4@gmail.com>
2021-04-19 21:16:56 -0400

    Add automatic update for navbar when user is logged in and remove unused .css files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -10,23 +10,25 @@ import Output from './Components/Output';
 import Signup from './Components/Signup';
 
 function App() {
-  return (
-      <Router>
-          <div>
-              <Navigation/>
-              <Switch>
-                  <Route path='/about' component={About}/>
-                  <Route path='/contact' component={Contact}/>
-                  <Route path='/help' component={Help}/>
-                  <Route path='/login' component={Login}/>
-                  <Route path='/model' component={Inputs}/>
-                  <Route path='/output' component={Output}/>
-                  <Route path='/signup' component={Signup}/>
-                  <Route path='/' component={Inputs}/>
-              </Switch>
-          </div>
-      </Router>
-  );
+    const [loggedIn] = useState(localStorage.getItem('access_token'));
+
+    return (
+        <Router>
+            <div>
+                <Navigation loggedIn={loggedIn}/>
+                <Switch>
+                    <Route path='/about' component={About}/>
+                    <Route path='/contact' component={Contact}/>
+                    <Route path='/help' component={Help}/>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/model' component={Inputs}/>
+                    <Route path='/output' component={Output}/>
+                    <Route path='/signup' component={Signup}/>
+                    <Route path='/' component={Inputs}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import axios_instance from '../axiosApi';
 
-function Navigation() {
+function Navigation(props) {
+    async function handleSignOut() {
+        try {
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            axios_instance.defaults.headers['Authorization'] = null;
+            return;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <Navbar bg="dark" variant="dark" expand="sm" >
             <Navbar.Brand href="model">Veritas Economics</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbar-nav"/>
             <Navbar.Collapse id="navbar-nav">
-                <Nav className="mr-auto">
+                <Nav className="container-fluid">
                     <Nav.Link href='model'>Choice Model</Nav.Link>
                     <Nav.Link href='contact'>Contact</Nav.Link>
                     <Nav.Link href='about'>About</Nav.Link>
                     <Nav.Link href='help'>Help</Nav.Link>
-                    <Nav.Link href='login'>Login</Nav.Link>
-                    <Nav.Link href='signup'>Signup</Nav.Link>
+                    { props.loggedIn ? null : <Nav.Link className='ml-auto' href='login'>Login</Nav.Link> }
+                    { props.loggedIn ? null : <Nav.Link href='signup'>Signup</Nav.Link> }
+                    { props.loggedIn ? <Nav.Link className='ml-auto' href='account'>My Account</Nav.Link> : null }
+                    { props.loggedIn ? <Button onClick={handleSignOut}>Sign Out</Button>: null }
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
