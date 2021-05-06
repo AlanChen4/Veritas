5177977b532e7cfe68e29dd203667a7e9f664835
Alan Chen <avchen4@gmail.com>
2021-05-05 23:18:39 -0400

    Add start to geomap

---
+++ src/Components/GeoMap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Sphere,
+    Graticule
+} from 'react-simple-maps';
+
+const GeoMap = () => {
+    return (
+        <ComposableMap>
+
+        </ComposableMap>
+    );
+};
+
+export default GeoMap;
