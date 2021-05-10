3ffa9546eaa344e86c551f4dff918529ef3d94ed
Alan Chen <avchen4@gmail.com>
2021-05-10 14:29:25 -0400

    Replace components with cards for less cluttered design

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
-import ModelBarChart from './ModelBarChart';
-import GeoMap from './GeoMap';
 
 const Output = (props) => {
     const location = useLocation();
     return (
         <div>
-            <GeoMap />
-            <ModelBarChart location={location} />
+            <h2></h2>
+            <Container fluid>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <Card>
+                            <Card.Img variant='top' src='tn_map.png' />
+                            <Card.Body>
+                                <Card.Title>Map Visualization</Card.Title>
+                                <Card.Text>
+                                    Choropleth map with each county shaded to represent total estimated households that will switch over.
+                                </Card.Text>
+                                <Button variant='primary'>Open Map</Button>            
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <Card>
+                            <Card.Img variant='top' src='model_bar_chart.png' />
+                            <Card.Body>
+                                <Card.Title>Bar Chart</Card.Title>
+                                <Card.Text>
+                                    Bar chart with zoom and toolbar to provide more in-depth comparison between the two offered plans.
+                                </Card.Text>
+                                <Button variant='primary'>Open Chart</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
