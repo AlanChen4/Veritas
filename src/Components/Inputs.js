2a53ab3c3f4ce36c4448f87156607a0c1be633b5
Alan Chen <avchen4@gmail.com>
2021-05-22 17:30:07 -0400

    Add feature to save inputs with a nickname

--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios_instance from '../axiosApi';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+import {
+    Button,
+    Col,
+    Form,
+    Modal
+} from 'react-bootstrap';
 import { OPTIONS } from './InputOptions';
 import './Inputs.css';
 
-function Inputs() {
+export default function Inputs() {
+    const history = useHistory();
+    const [errorMessage, setErrorMessage] = useState();
+
+    const [historyId, setHistoryId] = useState();
     const [offPeakA, setOffPeakA] = useState();
     const [offPeakB, setOffPeakB] = useState();
     const [peakPriceA, setPeakPriceA] = useState();
@@ -16,16 +23,66 @@ function Inputs() {
     const [peakPeriodB, setPeakPeriodB] = useState();
     const [peakSeasonA, setPeakSeasonA] = useState();
     const [peakSeasonB, setPeakSeasonB] = useState();
-    const history = useHistory();
-    const [errorMessage, setErrorMessage] = useState();
 
+    const [models, setModels] = useState([]);
+    const [selectedModel, setSelectedModel] = useState();
+
+    const [inputsNickname, setInputsNickname] = useState('');
+    const [showSaveInputs, setShowSaveInputs] = useState(false);
+    const handleCloseSaveInputs = () => {
+        setShowSaveInputs(false);
+    }
+    const handleSubmitNickname = async (e) => {
+        e.preventDefault();
+        if (inputsNickname === '') {
+            alert('Nickname can not be empty!');
+        } else {
+            setShowSaveInputs(false);
+            try {
+                await axios_instance.post('/api/choice-model/', 
+                    {
+                        history_id: historyId,
+                        nickname: inputsNickname,
+                        off_peak_a: offPeakA, off_peak_b: offPeakB,
+                        peak_a: peakPriceA, peak_b: peakPriceB,
+                        peak_period_a: peakPeriodA, peak_period_b: peakPeriodB,
+                        peak_season_a: peakSeasonA, peak_season_b: peakSeasonB
+                    }
+                );
+                setErrorMessage(false);
+            } catch (error) {
+                setErrorMessage('Inputs could not be saved. Please check that all inputs are filled in!');
+            }
+        }
+    }
+
+    const getProfileData = async () => {
+        const profileData = await axios_instance.get('/account/profile/');
+        return profileData;
+    }
+
+    const getModels = async (history_id) => {
+        const models = await axios_instance.get(`/account/${history_id}/models/`);
+        return models;
+    }
+
+    const getModelData = async (model_id) => {
+        const inputData = await axios_instance.get(`/api/choice-model/${model_id}/`);
+        setOffPeakA(inputData.data.off_peak_a);
+        setOffPeakB(inputData.data.off_peak_b);
+        setPeakPriceA(inputData.data.peak_a);
+        setPeakPriceB(inputData.data.peak_b);
+        setPeakPeriodA(inputData.data.peak_period_a);
+        setPeakPeriodB(inputData.data.peak_period_b);
+        setPeakSeasonA(inputData.data.peak_season_a);
+        setPeakSeasonB(inputData.data.peak_season_b);
+    }
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios_instance.post(`http://127.0.0.1:8000/api/choice-model/`,
-                // TODO: change so that history_id is gathered from current account
+            const response = await axios_instance.post(`/api/calculate/`,
                 {
-                    history_id: '7c722fb950264e768ed108f1ba6fbfc3',
+                    history_id: historyId,
                     off_peak_a: offPeakA,
                     off_peak_b: offPeakB,
                     peak_a: peakPriceA,
@@ -56,31 +113,69 @@ function Inputs() {
         }
     }
 
+    useEffect(() => {
+        getProfileData()
+        .then(response => {
+            const tempHistoryId = response.data.history_id;
+            setHistoryId(response.data.history_id);
+            getModels(tempHistoryId)
+            .then(response => {
+                setModels(response.data);
+            });
+        });
+
+    }, [showSaveInputs]);
+
     return (
         <div>
+            <Modal
+                animation={false}
+                show={showSaveInputs}
+                onHide={handleCloseSaveInputs}
+                backdrop='static'
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Save Inputs</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleSubmitNickname}>
+                        <Form.Group>
+                            <Form.Label>Nickname for these inputs</Form.Label>
+                            <Form.Control onChange={e => setInputsNickname(e.target.value)} placeholder='Enter nickname' value={inputsNickname} />
+                        </Form.Group>
+                        <Button type='submit'>
+                            Save Inputs
+                        </Button>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+            </Modal>
             {errorMessage && <div className='alert alert-danger'> {errorMessage} </div>}
             <Form className='inputs' onSubmit={handleSubmit}>
                 <Form.Row>
                     <Form.Group sm={2} as={Col}>
                         <Form.Label>Load Saved</Form.Label>
-                        <Form.Control as='select'>
-                            <option>Select</option>
+                        <Form.Control as='select' onChange={e => setSelectedModel(e.target.value)}>
+                            <option key='select'>Select</option>
+                            {models.map(m => 
+                                <option key={m.id} value={m.id}>{m.nickname}</option>)
+                            }
                         </Form.Control>
                     </Form.Group>
                     <Form.Group sm={1} as={Col} className='d-flex flex-column'>
-                        <Button className='load-btn mt-auto' variant='outline-primary'>Load</Button>
+                        <Button onClick={() => getModelData(selectedModel)} className='load-btn mt-auto' variant='outline-primary'>Load</Button>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Off-Peak Price (A)</Form.Label>
-                        <Form.Control as='select' onChange={e => setOffPeakA(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={offPeakA} onChange={e => setOffPeakA(e.target.value)}>
                             {OPTIONS.OFF_PEAK}
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Off-Peak Price (B)</Form.Label>
-                        <Form.Control as='select' onChange={e => setOffPeakB(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={offPeakB} onChange={e => setOffPeakB(e.target.value)}>
                             {OPTIONS.OFF_PEAK}
                         </Form.Control>
                     </Form.Group>
@@ -89,15 +184,13 @@ function Inputs() {
                     <Form.Group sm={3} as={Col}></Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Price (A)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakPriceA(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakPriceA} onChange={e => setPeakPriceA(e.target.value)}>
                             {OPTIONS.PEAK_PRICE}
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Price (B)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakPriceB(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakPriceB} onChange={e => setPeakPriceB(e.target.value)}>
                             {OPTIONS.PEAK_PRICE}
                         </Form.Control>
                     </Form.Group>
@@ -106,15 +199,13 @@ function Inputs() {
                     <Form.Group sm={3} as={Col}></Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Period (A)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakPeriodA(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakPeriodA} onChange={e => setPeakPeriodA(e.target.value)}>
                             {OPTIONS.PEAK_PERIOD}
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Period (B)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakPeriodB(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakPeriodB} onChange={e => setPeakPeriodB(e.target.value)}>
                             {OPTIONS.PEAK_PERIOD}
                         </Form.Control>
                     </Form.Group>
@@ -123,15 +214,13 @@ function Inputs() {
                     <Form.Group sm={3} as={Col}></Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Season (A)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakSeasonA(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakSeasonA} onChange={e => setPeakSeasonA(e.target.value)}>
                             {OPTIONS.PEAK_SEASON}
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Form.Label>Peak Season (B)</Form.Label>
-                        <Form.Control as='select' onChange={e => setPeakSeasonB(e.target.value)}>
-                            <option>Select</option>
+                        <Form.Control as='select' value={peakSeasonB} onChange={e => setPeakSeasonB(e.target.value)}>
                             {OPTIONS.PEAK_SEASON}
                         </Form.Control>
                     </Form.Group>
@@ -139,7 +228,7 @@ function Inputs() {
                 <Form.Row>
                     <Form.Group sm={3} as={Col}></Form.Group>
                     <Form.Group as={Col}>
-                        <Button variant='outline-secondary'>Save Inputs</Button>
+                        <Button onClick={() => setShowSaveInputs(true)}variant='outline-secondary'>Save Inputs</Button>
                     </Form.Group>
                     <Form.Group as={Col}>
                         <Button type='submit'>Calculate Model</Button>
@@ -149,5 +238,3 @@ function Inputs() {
         </div>
     );
 }
-
-export default Inputs;
