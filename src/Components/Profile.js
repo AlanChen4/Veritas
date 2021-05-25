2a87e22bee75621a72e63769d74e293eec214264
Alan Chen <avchen4@gmail.com>
2021-05-25 15:41:15 -0400

    Add ability to update profile and show alert after doing so

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form, Col } from 'react-bootstrap';
+import { 
+    Alert,
+    Button, 
+    Form, 
+    Col } from 'react-bootstrap';
 import axios_instance from '../axiosApi';
+import './Profile.css';
 
 const getProfileData = async () => {
     const profileData = await axios_instance.get('/account/profile/');
@@ -8,27 +13,29 @@ const getProfileData = async () => {
 }
 
 const Profile = () => {
-    const [profileData, setProfileData] = useState({
-        'history_id': '',
-        'email': '',
-        'username': '',
-        'first_name': '',
-        'last_name': '',
+    const [historyId, setHistoryId] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [updatedInfo, setUpdatedInfo] = useState(false);
 
-    });
-    const [email, setEmail] = useState();
-    const [username, setUsername] = useState();
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
+    const loadProfileData = async (data) => {
+        setHistoryId(data.history_id);
+        setEmail(data.email);
+        setUsername(data.username);
+        setFirstName(data.first_name);
+        setLastName(data.last_name);
+    }
 
     useEffect( () => {
         getProfileData()
         .then(response =>
-            setProfileData(response.data)
+            loadProfileData(response.data)
         );
     }, []);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const updatedData = {
@@ -38,46 +45,52 @@ const Profile = () => {
             'last_name': lastName
 
         }
-        console.log(updatedData);
+
+        await axios_instance.put(`/account/profile/${historyId}/`, updatedData);
+        setUpdatedInfo(true);
     }
 
-
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Row className='m-3'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    value={profileData.email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </Form.Row>
-            <Form.Row className='m-3'>
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                    value={profileData.username}
-                    onChange={e => setUsername(e.target.value)}
-                />
-            </Form.Row>
-            <Form.Row className='m-2'>
-                <Col xs={12} sm={6}>
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control 
-                        value={profileData.first_name} 
-                        onChange={e => setFirstName(e.target.value)}
-                    />
-                </Col>
-                <Col xs={12} sm={6}>
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control
-                        value={profileData.last_name}
-                        onChange={e => setLastName(e.target.value)}
-                    />
-                </Col>
-            </Form.Row>
-            <Button className='m-3' type='submit'>
-                Update Profile
-            </Button>
-        </Form>
+        <div>
+            {updatedInfo && <Alert variant='success'>Updated Profile!</Alert>}
+            <div className='profile-container'>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Row className='m-3'>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </Form.Row>
+                    <Form.Row className='m-3'>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                    </Form.Row>
+                    <Form.Row className='m-2'>
+                        <Col xs={12} sm={6}>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control 
+                                value={firstName} 
+                                onChange={e => setFirstName(e.target.value)}
+                            />
+                        </Col>
+                        <Col xs={12} sm={6}>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                value={lastName}
+                                onChange={e => setLastName(e.target.value)}
+                            />
+                        </Col>
+                    </Form.Row>
+                    <Button className='m-3 load-btn' variant='outline-primary' type='submit'>
+                        Update Profile
+                    </Button>
+                </Form>
+            </div>
+        </div>
     );
 }
 
