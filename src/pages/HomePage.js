f190afd09c4f79e24a9f8d5a648e4a821d14f8ea
Alan Chen <avchen4@gmail.com>
2021-06-07 22:45:58 -0400

    Add carousel to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import './HomePage.css';
 import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
 export default function HomePage() {
     const history = useHistory();
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    }
 
     const handleRouteChange = (route) => {
         history.push(route);
@@ -19,17 +28,37 @@ export default function HomePage() {
                     is capable of modeling your data onto both map and graph visualizations 
                     through specified inputs.
                 </p>
-                <Button className='load-btn mx-2' variant='outline-info' style={{color:'white'}} 
+                <Button className='load-btn mx-2' variant='info' style={{color:'white'}} 
                     onClick={() => handleRouteChange('model')} 
                 >
                     Model
                 </Button>
-                <Button className='load-btn mx-2' variant='outline-info' style={{color:'white'}} 
+                <Button className='load-btn mx-2' variant='info' style={{color:'white'}} 
                     onClick={() => handleRouteChange('signup')}
                 >
                     Sign Up
                 </Button>
             </Jumbotron>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        src='tn_map.png'
+                        alt='map visualization'
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        src='inputs.png'
+                        alt='inputs'
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        src='model_bar_chart.png'
+                        alt='bar chart'
+                    />
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
